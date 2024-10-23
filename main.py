5d2b056dd8bbad0b28ce06c021f0a6950613ccf1
Talhak <talhawkk@gmail.com>
2024-10-23 21:52:00 +0500

    adding speed method

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from time import *
 import random 
 def mistake(ptest,itest):
     error=0
+    itest=itest.split()
+    ptest=ptest.split()
     try:
         for i in range(len(ptest)):
             if ptest[i]!=itest[i]:
@@ -9,12 +11,11 @@ def mistake(ptest,itest):
     except:
         error+=1
     return error
-
-
-
-
-    
-
+def tspeed(t1,t2,user_input):
+    delay=t2-t1
+    delay1=round(delay,1)/60
+    word_count=len(user_input.split())
+    return round((word_count/delay1),1)
 tests=["Typing efficiently requires more than just speed. Its about finding a balance between accuracy and momentum",
       "A consistent typing practice can help improve both speed and accuracy.",
       "Every proficient typist understands the importance of maintaining good posture while typing.",
@@ -27,4 +28,6 @@ print(test,"\n\n")
 t11=time()
 testinput=input("Start here : ")
 t22=time()
+print("\nTyping speed is : ",tspeed(t11,t22,testinput))
+
 print(mistake(test,testinput))
